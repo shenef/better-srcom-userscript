f274e6f43ca56f877604dadef29b52e04dcd098b
shenef <matusik.rene@gmx.de>
2021-02-18 18:09:53 +0100

    Update better-srcom.user.js

--- better-srcom.user.js
+++ better-srcom.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         better speedrun.com
-// @version      0.1
 // @description  make speedrun.com prettier and fix some stuff
 // @author       shenef
+// @version      20210218.1
 // @match        https://www.speedrun.com/*
+// @resource     CSS https://raw.githubusercontent.com/shenef/better-srcom/main/style.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @resource     CSS https://raw.githubusercontent.com/shenef/better-srcom/main/style.css
 // @run-at       document-end
 // ==/UserScript==
 
